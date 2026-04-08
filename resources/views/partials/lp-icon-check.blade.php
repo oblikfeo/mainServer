99da1b92dbcca2925f8004aa77c0fe24e2a322e5
oblikfeo <oblikfeo@users.noreply.github.com>
2026-04-08 12:09:34 +0600

    Revert "Лендинг: ч/б иконки в фичах, тарифы колонками-карточками, маркеры в блоке оплаты"
    This reverts commit d0cc11e54056fa527aa075f33f1153744da48aee.

--- resources/views/partials/lp-icon-check.blade.php
+++
@@ -1,7 +0,0 @@
-{{-- Ч/б маркер для блока оплаты (обводка, без заливки) --}}
-<span class="lp-pay-icon" aria-hidden="true">
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-        <rect x="3" y="3" width="18" height="18" rx="1" />
-        <path d="M8 12.5l2.5 2.5 5-5" />
-    </svg>
-</span>
